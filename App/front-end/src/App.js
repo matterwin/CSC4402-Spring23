74501dcd246f2db89e2e3e63b4ebb85e9ce346c6
matterwin <matthewderwin23@gmail.com>
2023-02-24 01:26:42 -0600

    adding some basic styling and components

--- App/front-end/src/App.js
+++ App/front-end/src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from './Core/Home/Home'
+import Navbar from './Core/Nav/Navbar'
 
-import React from 'react'
+import React, { Component } from 'react'
 
 function App() {
   return (
-    <div className="App">
-      Movie Ratings
+    <div>
+       <Router>
+          
+          <Navbar /> 
+        
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+          </Routes>
+
+        </Router>
     </div>
   );
 }
--- App/front-end/src/Core/Nav/Navbar.js
+++ App/front-end/src/Core/Nav/Navbar.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
+import './Navbar.css'
+
 function Navbar() {
   return (
     <div>
-      Navbar
+      <div className="nav-container">
+        <h1>Navbar</h1>
+      </div>
     </div>
   );
 }
