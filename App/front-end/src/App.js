316dde4da3e8ac1e2253e8b767815eb76617337c
matterwin <matthewderwin23@gmail.com>
2023-02-24 02:09:40 -0600

    more edits to nav

--- App/front-end/src/App.js
+++ App/front-end/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from './Core/Home/Home'
 import Navbar from './Core/Nav/Navbar'
 
-import React, { Component } from 'react'
+import React from 'react'
 
 function App() {
   return (
--- App/front-end/src/Core/Nav/Navbar.js
+++ App/front-end/src/Core/Nav/Navbar.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 import './Navbar.css'
 
 function Navbar() {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+    const handleClick = () => {
+        setIsOpen(!isOpen);
+    }
+
+    function hideMenu() {
+        document.removeEventListener("click", hideMenu);
+        setIsOpen(false);
+    }
+
   return (
     <div>
       <div className="nav-container">
         <h1>Navbar</h1>
+        
+        <div className="menu-container">
+            <div className={`menu-icon ${isOpen ? 'menu-icon--open' : ''}`} onClick={handleClick} onMouseLeave={() => {
+            document.addEventListener("click", hideMenu)}}>
+                <div className="menu-icon__line"></div>
+                <div className="menu-icon__line"></div>
+                <div className="menu-icon__line"></div>
+            </div>                     
+        </div>
       </div>
     </div>
   );
