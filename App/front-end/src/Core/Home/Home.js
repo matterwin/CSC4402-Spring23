353ee1d54e71ec2c5c957534733311681f0e39d7
matterwin <matthewderwin23@gmail.com>
2023-02-26 12:45:14 -0600

    more edits

--- App/front-end/src/Core/Home/Home.js
+++ App/front-end/src/Core/Home/Home.js
@@ -8,35 +8,48 @@ function Home() {
       <div className="container">
         <div>
           <h1 className='home-title'>Movies reviewed, opinions shared</h1>
-          <p className="subtext-title">Discover new favorites and join the conversation with our user-driven movie review platform. Share your opinions and explore the diverse perspectives of our community. (This is ugly, will change later)
+          <p className="subtext-title">
+            Discover new favorites and join the conversation with our user-driven movie review platform. Share your opinions and explore the diverse perspectives of our community. (This is ugly, will change later)
           </p>
-        </div>
-
-        <div className="note-cards">
-          <div className="card">
-            hi
-          </div>
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <div className="card">
-            hi
-          </div>
+          <div className='space'>&nbsp;</div>
         </div>
       </div>
 
       <div className='space'>&nbsp;</div>
 
       <div className="container-bg-color">
+
+      <div className="note-cards">
+          <div className="card1">
+            hi
+          </div>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <div className="card1">
+            hi
+          </div>
+        </div>
+
+        <div className='space'>&nbsp;</div>
+
         <div>
-        Unlock the magic of the movies with our expert reviews and diverse perspectives, as we guide you through the world of cinema.
+          Unlock the magic of the movies with our expert reviews and diverse perspectives, as we guide you through the world of cinema.
         </div>
       </div>
 
       <div className='space'>&nbsp;</div>
-      <div className="container">
-        <div>
+      <div className="container-bottom">
+        {/* <div className='three-cards'>
+          
+        </div> */}
+        
+        <div className="stuff">
           Test
         </div>
-        
+        <div className='space'>&nbsp;</div>
+        <div className='space'>&nbsp;</div>
+        <div className='space'>&nbsp;</div>
+        <div className='space'>&nbsp;</div>
+        <div className='space'>&nbsp;</div>
       </div>
       <div className='space'>&nbsp;</div>
     </div>
