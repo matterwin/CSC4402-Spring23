7e29a68504c71adabe0967643dba75efa85323fb
Matthew Erwin <matthewderwin23@gmail.com>
2023-03-09 20:34:30 -0600

    adding more movies to see how it looks

--- App/front-end/src/Core/Feed/MovieCard.js
+++ App/front-end/src/Core/Feed/MovieCard.js
@@ -5,6 +5,11 @@ import StarsIcon from '@mui/icons-material/Stars';
 import screenPic from '../Videos/poe.jpg';
 import superbad from '../Videos/superbad.jpg';
 import fb from '../Videos/fb.jpg';
+import nuke from '../Videos/nuke.jpg';
+import mariobros from '../Videos/mariobros.jpg';
+import wolf from '../Videos/wolf.jpg';
+import transformers from '../Videos/transformers.jpg';
+import bear from '../Videos/bear.jpg';
 
 function MovieCard() {
 
@@ -41,63 +46,52 @@ function MovieCard() {
             <p className='last-review'>Last Review: 10/20/23</p>
         </div>  
         <div className='movie-review-div'>
-            <img alt="pic of movie" src={screenPic} className="movie-pic"></img>
+            <img alt="pic of movie" src={mariobros} className="movie-pic"></img>
             <StarsIcon sx={{color: "#1976d2"}}/>
             <StarsIcon sx={{color: "#1976d2"}}/>
             <StarsIcon sx={{color: "#1976d2"}}/>
             <StarsIcon sx={{color: "#1976d2"}}/>
             <StarsIcon sx={{color: "#1976d2"}}/>
-            <p className='movie-title'>Kung Fu Panda</p>
+            <p className='movie-title'>The Super Mariobros</p>
             <p className='last-review'>Last Review: 10/20/23</p>
         </div>  
         <div className='movie-review-div'>
-            <img alt="pic of movie" src={screenPic} className="movie-pic"></img>
+            <img alt="pic of movie" src={nuke} className="movie-pic"></img>
             <StarsIcon sx={{color: "#1976d2"}}/>
             <StarsIcon sx={{color: "#1976d2"}}/>
             <StarsIcon sx={{color: "#1976d2"}}/>
             <StarsIcon sx={{color: "#1976d2"}}/>
             <StarsIcon sx={{color: "#1976d2"}}/>
-            <p className='movie-title'>Kung Fu Panda</p>
+            <p className='movie-title'>Oppenheimer</p>
             <p className='last-review'>Last Review: 10/20/23</p>
         </div>  
         <div className='movie-review-div'>
-            <img alt="pic of movie" src={screenPic} className="movie-pic"></img>
+            <img alt="pic of movie" src={bear} className="movie-pic"></img>
             <StarsIcon sx={{color: "#1976d2"}}/>
             <StarsIcon sx={{color: "#1976d2"}}/>
             <StarsIcon sx={{color: "#1976d2"}}/>
             <StarsIcon sx={{color: "#1976d2"}}/>
             <StarsIcon sx={{color: "#1976d2"}}/>
-            <p className='movie-title'>Kung Fu Panda</p>
+            <p className='movie-title'>Cocaine Bear</p>
             <p className='last-review'>Last Review: 10/20/23</p>
         </div>
         <div className='movie-review-div'>
-            <img alt="pic of movie" src={screenPic} className="movie-pic"></img>
+            <img alt="pic of movie" src={wolf} className="movie-pic"></img>
             <StarsIcon sx={{color: "#1976d2"}}/>
             <StarsIcon sx={{color: "#1976d2"}}/>
             <StarsIcon sx={{color: "#1976d2"}}/>
             <StarsIcon sx={{color: "#1976d2"}}/>
-            <StarsIcon sx={{color: "#1976d2"}}/>
-            <p className='movie-title'>Kung Fu Panda</p>
+            <p className='movie-title'>The Wolf of Wallstreet</p>
             <p className='last-review'>Last Review: 10/20/23</p>
         </div>  
         <div className='movie-review-div'>
-            <img alt="pic of movie" src={screenPic} className="movie-pic"></img>
+            <img alt="pic of movie" src={transformers} className="movie-pic"></img>
             <StarsIcon sx={{color: "#1976d2"}}/>
             <StarsIcon sx={{color: "#1976d2"}}/>
             <StarsIcon sx={{color: "#1976d2"}}/>
             <StarsIcon sx={{color: "#1976d2"}}/>
             <StarsIcon sx={{color: "#1976d2"}}/>
-            <p className='movie-title'>Kung Fu Panda</p>
-            <p className='last-review'>Last Review: 10/20/23</p>
-        </div>  
-        <div className='movie-review-div'>
-            <img alt="pic of movie" src={screenPic} className="movie-pic"></img>
-            <StarsIcon sx={{color: "#1976d2"}}/>
-            <StarsIcon sx={{color: "#1976d2"}}/>
-            <StarsIcon sx={{color: "#1976d2"}}/>
-            <StarsIcon sx={{color: "#1976d2"}}/>
-            <StarsIcon sx={{color: "#1976d2"}}/>
-            <p className='movie-title'>Kung Fu Panda</p>
+            <p className='movie-title'>Transformers: Rise of The Beasts</p>
             <p className='last-review'>Last Review: 10/20/23</p>
         </div>  
     </div>
