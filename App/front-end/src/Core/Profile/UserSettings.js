a0c3f8370958808be7c88fb7eefadf82061ae131
Matthew Erwin <matthewderwin23@gmail.com>
2023-03-23 17:07:39 -0500

    started settings page

--- App/front-end/src/Core/Profile/UserSettings.js
+++ App/front-end/src/Core/Profile/UserSettings.js
@@ -1,16 +1,45 @@
-import React from 'react';
+import * as React from 'react';
+import Pika from "../Videos/pika.png";
+import Tooltip from '@mui/material/Tooltip';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import UploadButtons from './UploadButtons';
+
 
 import './UserSettings.css'
 
 function UserSettings() {
 
   return (
-    <div >
-        <div className="profile-container">
-            <p className='hello-there'>Welcome to your settings page</p>
-            <div className='space'>&nbsp;</div>
-            <div className='space'>&nbsp;</div>
-            <div className='space'>&nbsp;</div>
+    <div className='profile-container'>
+        <div className="setting-info-title">
+          <h2 className='reviews'>Profile Settings</h2>
+        </div>
+        <div className="setting-info">
+            <div className='settings-pic-div'>
+                <div className="settings-pfp-div">             
+                    <Tooltip title="Profile pic">
+                        <img className="prof-profile-pic" src={Pika} alt="ProfilePicture" />      
+                    </Tooltip>                            
+                </div>
+                <div className='upload-btn'>
+                  <UploadButtons />
+                </div>
+            </div>
+            
+            <div className='small-info'>
+                <div className='settings-small-row'>
+                  <TextField id="outlined-basic" placeholder='Full name' variant="outlined" />
+                   
+                </div>
+                <div className='settings-small-row'>
+                  <TextField id="outlined-basic" placeholder='Tell us what movies interest you' variant="outlined" />
+                </div>
+                
+            </div>
+        </div> 
+        <div className="setting-info">
+              <h2 className='reviews'>Change username</h2>
         </div>
     </div>
   );
