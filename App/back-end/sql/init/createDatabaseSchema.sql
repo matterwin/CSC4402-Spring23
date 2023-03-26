b9c10118d20eaf55c2daa348b48ca1c1bb361eb5
Matthew Erwin <matthewderwin23@gmail.com>
2023-03-25 21:03:07 -0500

    adding UNIQUE to userAuthTable for username

--- App/back-end/sql/init/createDatabaseSchema.sql
+++ App/back-end/sql/init/createDatabaseSchema.sql
@@ -11,7 +11,7 @@ CREATE TABLE testTable(
 
 CREATE TABLE userAuthTable(
     id INT UNIQUE NOT NULL AUTO_INCREMENT,
-    username VARCHAR(255) NOT NULL,
+    username VARCHAR(255) UNIQUE NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     
