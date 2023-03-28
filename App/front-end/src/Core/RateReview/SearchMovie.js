bbea3c8b6c8065fbd7a49af7b7953e33f534ee60
theassembler1 <lewisnoah592@gmail.com>
2023-03-28 16:33:52 -0500

    added movie names to search

--- App/front-end/src/Core/RateReview/SearchMovie.js
+++ App/front-end/src/Core/RateReview/SearchMovie.js
@@ -1,15 +1,28 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 
-import { top100Films } from './SearchMovieIndex';
-
 export default function ComboBox() {
+  const [movieNames, setMovieNames] = useState(undefined);
+
+  useEffect(() => {
+    fetch('http://localhost:8000/api/movieControllerName')
+      .then(result => result.json())
+      .then(json => setMovieNames(json))
+      .catch(err => console.error(err));
+  }, []);
+
+  if(!movieNames) {
+    return (
+      <h1>Loading</h1>
+    );
+  }
+
   return (
     <Autocomplete
       disablePortal
       id="combo-box-demo"
-      options={top100Films}
+      options={movieNames}
       sx={{ width: 300 }}
       renderInput={(params) => <TextField {...params} label="Movie" />}
     />
