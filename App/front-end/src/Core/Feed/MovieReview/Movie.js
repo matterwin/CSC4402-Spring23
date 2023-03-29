f6613cd66182f76bff13f7f5e412e8659438ad5f
Matthew Erwin <matthewderwin23@gmail.com>
2023-03-29 01:59:21 -0500

    more edits before someone pulls

--- App/front-end/src/Core/Feed/MovieReview/Movie.js
+++ App/front-end/src/Core/Feed/MovieReview/Movie.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
+import '../MovieCard.css';
+// import StarsIcon from '@mui/icons-material/Stars';
+import screenPic from '../../Videos/poe.jpg';
+import Stars from '../Stars';
+
 import './Movie.css'
 
 function Movie() {
@@ -15,7 +20,25 @@ function Movie() {
           </div>
         </div>
       <div className='movie-container'>
-        <h1>Hello</h1>
+        <p>&nbsp;</p>
+        <div className='movie-and-info'>
+          <div className='movie-div'>
+              <img alt="pic of movie" src={screenPic} className="review-movie-pic"></img>        
+          </div>
+          <div className='information'>
+            <div className='info-div'>
+              <div>
+                <Stars value={4.5}/>
+              </div>
+              <div>
+                <p className='review-movie-title'>Kung Fu Panda</p>
+              </div>
+              <div>
+                <p className='review-last-review'>Released on: 12/2/23</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       </div>
     </div>
---
+++ App/front-end/src/Core/Feed/MovieReview/UserReviews.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './UserReviews.css'
+
+function UserReviews() {
+
+  return (
+    <div>
+      <p>This is where the user reviews for a particular movie will be.</p>
+      <p>You will import this into Movie.js</p>
+    </div>
+  );
+}
+
+export default UserReviews;
