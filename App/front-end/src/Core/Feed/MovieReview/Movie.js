d39ca3a994ae647716419e8889f2f4ffd6fd2b33
Matthew Erwin <matthewderwin23@gmail.com>
2023-03-29 12:46:10 -0500

    going to sleep

--- App/front-end/src/Core/Feed/MovieReview/Movie.js
+++ App/front-end/src/Core/Feed/MovieReview/Movie.js
@@ -20,24 +20,35 @@ function Movie() {
           </div>
         </div>
       <div className='movie-container'>
-        <p>&nbsp;</p>
-        <div className='movie-and-info'>
+        <div className='movie-and-info-div'>
           <div className='movie-div'>
               <img alt="pic of movie" src={screenPic} className="review-movie-pic"></img>        
           </div>
           <div className='information'>
             <div className='info-div'>
-              <div>
+              <div className='info-heading'>
+                <p>Kung Fu Panda</p>
+              </div>
+              <div className='stars-div'>
                 <Stars value={4.5}/>
               </div>
               <div>
-                <p className='review-movie-title'>Kung Fu Panda</p>
+                <p className='review-movie-title'><b>Genre: </b>Comedy</p>
               </div>
               <div>
-                <p className='review-last-review'>Released on: 12/2/23</p>
+                <p className='review-movie-title'><b>Description: </b>An oddball group of cops, criminals, tourists and teens converge on a Georgia forest where a huge black bear goes on a murderous rampage after unintentionally ingesting cocaine.</p>
+              </div>
+              <div>
+                <p className='review-movie-title'><b>Length: </b>1:25:0</p>
+              </div>
+              <div>
+                <p className='review-movie-title'><b>Released on: </b>  12/2/23</p>
               </div>
             </div>
           </div>
+        </div>
+        <div>
+          Comment Section
         </div>
       </div>
       </div>
