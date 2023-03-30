2fa8148603c66645e04a27421c6428e45716ac63
Matthew Erwin <matthewderwin23@gmail.com>
2023-03-29 21:32:22 -0500

    pushing john cena

--- App/front-end/src/Core/Feed/MovieCard.js
+++ App/front-end/src/Core/Feed/MovieCard.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import './MovieCard.css';
 import Stars from './Stars';
 import Loading from '../Loading/Loading'
+import Unknown from '../Videos/johncena.png';
 
 function MovieCard() {
   const [movies, setMovies] = useState(undefined);
@@ -27,7 +28,15 @@ function MovieCard() {
   movies.forEach((movie) => {
     jsxMovies.push(
         <div key={ movie.name } className='movie-review-div' data={ movie.id }>
-            <img alt={ movie.name } src={ movie.filepath } className="movie-pic"></img>
+            <img 
+              src={ movie.filepath } 
+              alt={ movie.name } 
+              onError={(e) => {
+                e.target.onerror = null; 
+                e.target.src = Unknown;
+              }} 
+              className="movie-pic"
+            />
             <Stars value={movie.avg}/>
             <p className='movie-title'>{ movie.name }</p>
             <p className='last-review'>Released on: { movie.releaseDate }</p>
