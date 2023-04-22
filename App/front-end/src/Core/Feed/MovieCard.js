addd0b4569eba90e1193964ee260176e502c6593
Matthew Erwin <matthewderwin23@gmail.com>
2023-04-22 03:27:06 -0500

    should have went to bed

--- App/front-end/src/Core/Feed/MovieCard.js
+++ App/front-end/src/Core/Feed/MovieCard.js
@@ -1,16 +1,16 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import './MovieCard.css';
 import Stars from './Stars';
 import Loading from '../Loading/Loading'
-// import Unknown from '../Videos/johncena.png';
 import Unknown from '../Videos/superbad.jpg';
 import { useNavigate } from "react-router-dom";
-// import Tooltip from '@mui/material/Tooltip';
+import VisibilityIcon from '@mui/icons-material/Visibility';
 
 function MovieCard(props) {
   const navigate = useNavigate();
   const movies = props.movies;
+  const [isHovering, setIsHovering] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false);
 
   if(!props.movies) {
     return (
@@ -27,48 +27,65 @@ function MovieCard(props) {
   }
 
   movies.forEach((movie) => {
+
+    setTimeout(() => {
+      setIsLoaded(true);
+    }, 50 * movie.index);
+    
     jsxMovies.push(
-        <div key={ movie.name } className='movie-review-div' data={ movie.id }>
-          {/* <Tooltip title={toolTipMsg}> */}
-            <img 
-              src={ movie.filepath } 
-              alt={ movie.name } 
-              onClick={() => {
-                onClick(movie.id); 
-              }}
-              onError={(e) => {
-                e.target.onerror = null; 
-                e.target.src = Unknown;
-                // setShowToolTip(true);
-              }} 
-              className="movie-pic"
-            />
-            {/* </Tooltip> */}
-            <div className='rating-in-movie-div' onClick={() => {
-                onClick(movie.id); 
-              }}>
-              <Stars value={movie.avg}/>
-              <div className='percentage'>
-                {(movie.avg/5*100).toFixed(0)}%
-              </div>
-            </div>
-            <p 
-              onClick={() => {
-                onClick(movie.id); 
-              }}
-              className='movie-title'
-            >
-              { movie.name }
-            </p>
-            <p 
-              onClick={() => {
-                onClick(movie.id); 
-              }}
-              className='last-review'
-            >
-              Released { movie.releaseDate.replace(/-/g, " ") }
-            </p>
+      <div 
+        key={ movie.name } 
+        className={`movie-review-div ${isLoaded ? 'fade-in' : ''}`} 
+        data={ movie.id } 
+        onMouseEnter={() => setIsHovering((prevState) => ({ ...prevState, [movie.id]: true }))}
+        onMouseLeave={() => setIsHovering((prevState) => ({ ...prevState, [movie.id]: false }))}
+      >
+        {isHovering[movie.id] && (
+        <div className='remove-redeye-icon'>
+          <VisibilityIcon
+            sx={{ fontSize:'40px', color: '#fff', padding:0, marginBottom: '-3px' }} 
+            onClick={() => {
+              onClick(movie.id); 
+            }}/>
         </div>
+        )}
+        <img 
+          src={ movie.filepath } 
+          alt={ movie.name } 
+          onClick={() => {
+            onClick(movie.id); 
+          }}
+          onError={(e) => {
+            e.target.onerror = null; 
+            e.target.src = Unknown;
+          }} 
+          className="movie-pic"
+        />
+        <div className='rating-in-movie-div' onClick={() => {
+            onClick(movie.id); 
+          }}>
+          <Stars value={movie.avg}/>
+          <div className='percentage'>
+            {(movie.avg/5*100).toFixed(0)}%
+          </div>
+        </div>
+        <p 
+          onClick={() => {
+            onClick(movie.id); 
+          }}
+          className='movie-title'
+        >
+          { movie.name }
+        </p>
+        <p 
+          onClick={() => {
+            onClick(movie.id); 
+          }}
+          className='last-review'
+        >
+          Released { movie.releaseDate.replace(/-/g, " ") }
+        </p>
+      </div>
     );
   });
 
